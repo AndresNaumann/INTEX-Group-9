8fecdb8842b4aa8212d3602c580c8a8f035db622
Andrew Naumann <andnau702@gmail.com>
2024-04-08 13:09:05 -0600

    more stuff added

--- BrickwellStore/Program.cs
+++ BrickwellStore/Program.cs
@@ -1,14 +1,13 @@
 using BrickwellStore.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using BrickwellStore.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
