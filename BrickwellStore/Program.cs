01de911372f068bfcf379297fdd6037a9ff60b0a
Andrew Naumann <andnau702@gmail.com>
2024-04-08 12:31:39 -0600

    more identity stuff

--- BrickwellStore/Program.cs
+++ BrickwellStore/Program.cs
@@ -1,6 +1,7 @@
 using BrickwellStore.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using BrickwellStore.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
