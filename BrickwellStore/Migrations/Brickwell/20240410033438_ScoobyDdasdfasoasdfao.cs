8f87474967c3590d8e47657bf386c73abb6162d6
Andrew Naumann <andnau702@gmail.com>
2024-04-09 21:36:18 -0600

    here you go

---
+++ BrickwellStore/Migrations/Brickwell/20240410033438_ScoobyDdasdfasoasdfao.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrickwellStore.Migrations.Brickwell
+{
+    /// <inheritdoc />
+    public partial class ScoobyDdasdfasoasdfao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerRecommendations",
+                columns: table => new
+                {
+                    customerId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    recommendedProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerRecommendations", x => x.customerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductRecommendations",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RecommendedProductID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductRecommendations", x => x.ProductID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerRecommendations");
+
+            migrationBuilder.DropTable(
+                name: "ProductRecommendations");
+        }
+    }
+}
