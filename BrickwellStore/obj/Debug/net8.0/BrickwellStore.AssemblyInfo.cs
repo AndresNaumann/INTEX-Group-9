1a4e5c0be3c5f94c197f08ec48ddfa9601b81be6
sadieeyre <107454363+sadieeyre@users.noreply.github.com>
2024-04-10 13:11:06 -0600

    admin pages only accessible if logged in

--- BrickwellStore/obj/Debug/net8.0/BrickwellStore.AssemblyInfo.cs
+++ BrickwellStore/obj/Debug/net8.0/BrickwellStore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrickwellStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef65906b922aab45404f94dd2c9877ac9f760fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e9a33e0895d85cbec60eff1e26f5562a0eabf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrickwellStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrickwellStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
